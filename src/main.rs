0071b2f8447a138fe50c028837b31bac3da04ff2
mahavir-dash <mahavir@swnglobal.com>
2024-06-25 20:53:37 +0530

    Completed question 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+// Add two numbers
+fn add(a: String, b : String) -> String{
+     return a * b 
+}
+
+// Description of the question
+fn q2(){
+    todo!()
+}
