968474a898b106cb7bace4314c7194b28ab18613
rajiknows <rajeshhjhamain@gmail.com>
2024-06-25 21:44:06 +0530

    fixed the func

---
+++ target/debug/deps/demo_project_SIT-444d874fd340df8e.d
@@ -0,0 +1,5 @@
+/home/raj/programming/rust-internsh/demo-project-SIT/target/debug/deps/libdemo_project_SIT-444d874fd340df8e.rmeta: src/main.rs
+
+/home/raj/programming/rust-internsh/demo-project-SIT/target/debug/deps/demo_project_SIT-444d874fd340df8e.d: src/main.rs
+
+src/main.rs:
