39ba9a662074aeb35103e48a784d5ceb53cde0a1
sambit-95 <ece.23becf81@silicon.ac.in>
2024-06-27 12:14:35 +0530

    modify demo

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello new  world!");
 }
 
 
 // Add two numbers
 fn add(a: String, b : String) -> String{
-     return a * b 
+     return a * b; 
+     return a + b;
 }
 
 // Description of the question
 fn q2(){
-    todo!()
+    
+
 }
