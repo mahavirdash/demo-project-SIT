973f7e4e616f0bb93a5d38e5afa8bcec8ac234f3
anurag <anurag891mishra@gmail.com>
2024-06-28 12:07:53 +0530

    canged some minute details as they were causing error

---
+++ src/rust1.rs
@@ -0,0 +1,21 @@
+fn main()
+{
+    let mut x :u64 = 1000;
+    println!("the value befor changing x is {}",x);
+    let x :u64 = 109;
+    println!("the value befor changing x is {}",x);
+    condition();
+}
+fn condition()
+{
+    let a =89;
+    if a==1
+    {
+        println!("The value of a is {}",a);
+    }
+    else 
+    {
+        println!("The value of a is not 1")
+    }
+}
+fn tup()
